ec101c31e598a13664efc6ff995678b3720c3509
Mete Balci <126746772+metebalc@users.noreply.github.com>
2024-10-27 03:01:03 -0700

    Update StringToInteger(atoi).cpp

--- solutions/4_string_to_integer/StringToInteger(atoi).cpp
+++ solutions/4_string_to_integer/StringToInteger(atoi).cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, size = s.size();
+        int sign = 1;
+        int result = 0;
 
+        // Skip leading whitespaces
+        while (i < size && s[i] == ' ')
+        {
+            ++i;
+        }
+
+        if (i < size && (s[i] == '+' || s[i] == '-')) 
+        {
+            sign = (s[i] == '-') ? -1 : 1;
+            ++i;
+        }
+
+        // Convert digits to integer
+        while (i < size && s[i] >= '0' && s[i] <= '9') 
+        {
+            int digit = s[i] - '0';
+
+            // Check for overflow/underflow before updating result
+            if (result > (INT_MAX - digit) / 10) 
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
+            ++i;
+        }
+
+        return result * sign;
+    }
+};
